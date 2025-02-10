1246dd22dee64344a06de5dd0ec7deeac34805ef
1dans <idansuwu@gmail.com>
2025-02-10 15:38:36 +0200

    сделал матрицу для пятнашек

--- final.cpp
+++ final.cpp
@@ -2,10 +2,33 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#define E endl;
+#define T cout
+using namespace std;
+
+
+void menu() { //П'ятнашки
+    T << "П'ятнашки" << E
+    vector<vector<int>> matrix(3, vector<int>(3));
+    vector<int> numbers;
+    for (int i = 1; i <= 8; ++i) numbers.push_back(i);
+    numbers.push_back(0);
+    random_shuffle(numbers.begin(), numbers.end());
+    int index = 0;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            matrix[i][j] = numbers[index++];
+            T << matrix[i][j] << ' ';
+        }
+        T << E
+    }
+}
+
 
 int main()
 {
+    srand(time(NULL));
     system("chcp 1251>null");
-    cout << " ";
+    menu();
 }
 
